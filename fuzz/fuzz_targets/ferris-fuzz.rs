817774b15fad29f1034fbf5c9dbd745bf3e0a8ec
ysthakur <2004yst@gmail.com>
2022-12-21 13:42:13 -0500

    Restrict max width

--- fuzz/fuzz_targets/ferris-fuzz.rs
+++ fuzz/fuzz_targets/ferris-fuzz.rs
@@ -6,10 +6,24 @@ use libfuzzer_sys::fuzz_target;
 use ferris_says::say;
 use std::io::BufWriter;
 
-#[derive(Arbitrary, Debug)]
+#[derive(Debug)]
 struct FuzzInput {
     input: String,
     width: usize,
+}
+
+const MAX_WIDTH: usize = 100;
+
+impl<'a> Arbitrary<'a> for FuzzInput {
+    fn arbitrary(u: &mut arbitrary::Unstructured<'a>) -> arbitrary::Result<Self> {
+        let width = u.int_in_range(0..=MAX_WIDTH)?;
+        let input_size = u.arbitrary_len::<char>()?;
+        let mut input = String::new();
+        for _ in 0..=input_size {
+            input.push(u.arbitrary()?);
+        }
+        Ok(FuzzInput { input, width })
+    }
 }
 
 fuzz_target!(|data: FuzzInput| {
